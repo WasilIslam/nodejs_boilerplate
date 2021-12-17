2a9e3cfd5820d3295289f9143da6e67dc6c54d72
wasilislam <wasilislam456@gmail.com>
2021-12-17 10:16:37 +0500

    second commit

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const app = require("express")();
 require("dotenv").config();
 require("./routes")(app);
 
-const {PORT,db}=process.env;
-
-
+const {PORT, db} = process.env;
 
 mongoose
   .connect(db)
   .then(() => console.log("Connected to: ", db))
-  .catch((err) => console.log("Cannot connect to the database Error:",err));
+  .catch((err) => console.log("Cannot connect to the database Error:", err));
 
-app.listen(PORT||5000, (err) => {
-  if (!err) console.log("Listening to port "+ PORT);
+app.get("/", (req, res) => {
+  res.send("Hello there i am working!!!");
+});
+
+app.listen(PORT || 5000, (err) => {
+  if (!err) console.log("Listening to port " + PORT);
 });
